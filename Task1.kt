301ce97fad2db1be921f689646dbcbd65e14354e
Никита Кошманов <31765932+NikitaKoshmanov@users.noreply.github.com>
2021-03-31 16:46:52 +0300

    Create Task1.kt

---
+++ Task1.kt
@@ -0,0 +1,23 @@
+fun main(args: Array<String>) {
+    val input = readLine()
+    var empty = ""
+    var count = 0
+    if(input != null) {
+        val str = input.toLowerCase().toCharArray()
+        var result = empty.padEnd(str.size, '(').toCharArray()
+        for(i in 0 until str.size){
+            var flag = str[i]
+            for(j in i+1 until str.size){
+                if(str[j] == flag){
+                    result[j] = ')'
+                    ++count
+                }
+                if(count > 0) result[i] = ')'
+                count = 0
+            }
+        }
+        println(str)
+        println(result)
+    }
+
+}
